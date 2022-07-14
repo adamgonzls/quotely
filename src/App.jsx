c9c21d50cab91c01e4e208b38c7d0e23d47eb399
Adam Gonzales <adamgonzales1@gmail.com>
2022-07-13 21:28:02 -0700

    fix: update randomQuoteObj props except backgroundImageURL

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,14 @@ function App() {
     id: null,
     profession: '',
     quote: 'Growth demands a temporary surrender of security',
-    backgroundImageURL: 'https://images.unsplash.com/photo-1546448396-6aef801',
+    backgroundImageURL:
+      'https://images.unsplash.com/photo-1545431781-3e1b506e9a37?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNDU3MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc3NzE0NjY&ixlib=rb-1.2.1&q=80&w=1080',
   })
   const [allQuotesData, setAllQuotesData] = useState([])
-  // const [randomBackgroundImage, setRandomBackgroundImage] = useState(
-  // 'https://images.unsplash.com/photo-1546448396-6aef801'
-  // )
+  const [unsplashImagesArray, setUnsplashImagesArray] = useState([])
 
-  // console.log(randomBackgroundImage)
+  console.log(randomQuoteObj)
+  console.log(unsplashImagesArray)
 
   useEffect(() => {
     fetch(`https://adams-quote-api-v1.herokuapp.com/quotes`)
@@ -25,42 +25,53 @@ function App() {
   }, [])
 
   useEffect(() => {
+    console.log('initial call to unsplash API')
+    fetch(
+      `https://api.unsplash.com/photos/random/?client_id=RyjdpW1hxq8mSR8fAHEE8pUNx38fSXjgdnd0UeBCVu4&query=abstract&count=30`
+    )
+      .then((res) => res.json())
+      .then((data) => setUnsplashImagesArray(data))
     // getRandomUnsplashImage()
   }, [])
 
   useEffect(() => {
     const interval = setInterval(() => {
-      // getRandomUnsplashImage()
-      console.log('called')
+      console.log('interval call to getRandomUnsplashImage()')
+      getRandomUnsplashImage()
     }, 20000)
     return () => clearInterval(interval)
   }, [])
 
-  function getRandomUnsplashImage() {
-    fetch(
-      `https://api.unsplash.com/photos/random/?client_id=RyjdpW1hxq8mSR8fAHEE8pUNx38fSXjgdnd0UeBCVu4&query=abstract&count=1`
-    )
-      .then((res) => res.json())
-      .then((data) =>
-        setRandomQuoteObj((prevData) => {
-          return {
-            ...prevData,
-            backgroundImageURL: data[0].urls.regular,
-          }
-        })
-      )
-  }
-
   function handleGetRandomQuote() {
     const randomQuoteData =
       allQuotesData[Math.floor(Math.random() * allQuotesData.length)]
+    console.log(randomQuoteData)
     setRandomQuoteObj((prevData) => {
       return {
         ...prevData,
-        author,
-        id,
-        profession,
-        quote,
+        author: randomQuoteData.author ? randomQuoteData.author : '',
+        id: randomQuoteData.id,
+        profession: randomQuoteData.profession
+          ? randomQuoteData.profession
+          : '',
+        quote: randomQuoteData.quote,
+      }
+    })
+  }
+
+  function getRandomUnsplashImage() {
+    console.log('give me a random image')
+    const randomImage =
+      unsplashImagesArray[
+        Math.floor(Math.random() * unsplashImagesArray.length)
+      ]
+    console.log(unsplashImagesArray)
+    console.log(randomImage.urls.regular)
+    const randomImageURL = randomImage.urls.regular
+    setRandomQuoteObj((prevData) => {
+      return {
+        ...prevData,
+        backgroundImageURL: randomImageURL,
       }
     })
   }
